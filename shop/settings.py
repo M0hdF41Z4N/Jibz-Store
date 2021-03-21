1acf6858dadf338dc70ff67a66221d7a4ac7ead9
M0hdF41Z4N <31098815+M0hdF41Z4N@users.noreply.github.com>
2021-03-22 00:04:10 +0700

    Update settings.py

--- shop/settings.py
+++ shop/settings.py
@@ -28,7 +28,7 @@ STRIPE_SECRET_KEY = "sk_test_4eC39HqLyjWDarjtT1zdp7dc"
 #STRIPE_SECRET_KEY = config('STRIPE_LIVE_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','jibz-store.herokuapp.com']
 
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
